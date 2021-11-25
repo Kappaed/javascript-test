568f60793f348b78748a0311db9abc26c4ec33b2
Kalifunk <nathansing.ns@gmail.com>
2021-11-25 13:59:41 +1100

    added bg image

--- part3/src/App.js
+++ part3/src/App.js
@@ -1,12 +1,20 @@
 import "./App.css";
 import styled from "styled-components";
 import Header from "./components/header/Header";
+import bgImage from "./images/home1-bg.jpg";
 
 const AccordionWrapper = styled.div`
   display: flex;
   position: relative;
-  &:after {
-  }
+`;
+
+const ImgWrapper = styled.img`
+  position: absolute;
+  width: 100%;
+  top: 0;
+  left: 0;
+  height: 100vh;
+  z-index: -10;
 `;
 
 function App() {
@@ -14,6 +22,7 @@ function App() {
     <div className="App">
       <AccordionWrapper>
         <Header />
+        <ImgWrapper src={bgImage} />
       </AccordionWrapper>
     </div>
   );
